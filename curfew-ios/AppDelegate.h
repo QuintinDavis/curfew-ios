2cf74f2716f0c7d5889b79a77740d17f3ca61daa
Luke Reichold <lukereichold@gmail.com>
2015-01-31 16:34:46 -0600

    Initial Commit

---
+++ curfew-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  curfew-ios
+//
+//  Created by Luke Reichold on 1/31/15.
+//  Copyright (c) 2015 Curfew CS 390 Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
